72e54b30e34b943106060ba09abbcefedebe5c0f
Lauren <andersonl5@mymail.shawnee.edu>
2024-01-29 10:43:11 -0500

    fixing stuff

--- src/Index.py
+++ src/Index.py
@@ -5,4 +5,5 @@ class Handler(tornado.web.RequestHandler):
         self.write('<a href="/profile/alice">alice</a><br>')
         self.write('<a href="/profile/bob">bob</a><br>')
         self.write('<a href="/profile/carol">carol</a><br>')
-        self.write('<a href="/profile/dave">dave</a><br>')                        
+        self.write('<a href="/profile/dave">dave</a><br>')  
+        self.write('<a href="/static/rou.html">Click to enter Casino</a><br>')                      
